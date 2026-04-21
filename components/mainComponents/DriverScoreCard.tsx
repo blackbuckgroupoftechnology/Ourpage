3b6dfe31ff0ae00bc9e4ed71c5861e09cdc9cfa1
Amgain <surajamgain9@gmail.com>
2026-04-21 12:09:12 +0545

    deployment ready

--- components/mainComponents/DriverScoreCard.tsx
+++ components/mainComponents/DriverScoreCard.tsx
@@ -1,98 +1,98 @@
-"use client";
+// "use client";
 
-import React from 'react'
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  Legend,
-  ArcElement
-} from "chart.js";
+// import React from 'react'
+// import {
+//   Chart as ChartJS,
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   Tooltip,
+//   Legend,
+//   ArcElement
+// } from "chart.js";
 
-// Register components
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  ArcElement,
-  Tooltip,
-  Legend
-);
-import { Line, Pie } from 'react-chartjs-2';
+// // Register components
+// ChartJS.register(
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   ArcElement,
+//   Tooltip,
+//   Legend
+// );
+// import { Line, Pie } from 'react-chartjs-2';
 
-const PieData = {
-  labels: ["High Risk", "Average Risk", "Low Risk"],
-  datasets: [{
-    data: [10, 40, 43],
-    backgroundColor: ["#faa693", "#fae993", "#8fc2f7"],
-  },
-  ],
-};
-const pieOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: "right",
-      labels: {
-        color: "black",
-        font: {
-          size: 12,
-        },
-        padding: 20,
-        boxWidth: 20,
-        boxHeight: 20 // size of color box
-      },
-    },
-  },
-};
+// const PieData = {
+//   labels: ["High Risk", "Average Risk", "Low Risk"],
+//   datasets: [{
+//     data: [10, 40, 43],
+//     backgroundColor: ["#faa693", "#fae993", "#8fc2f7"],
+//   },
+//   ],
+// };
+// const pieOptions = {
+//   responsive: true,
+//   maintainAspectRatio: false,
+//   plugins: {
+//     legend: {
+//       position: "right",
+//       labels: {
+//         color: "black",
+//         font: {
+//           size: 12,
+//         },
+//         padding: 20,
+//         boxWidth: 20,
+//         boxHeight: 20 // size of color box
+//       },
+//     },
+//   },
+// };
 
-const lineData = {
-  labels: ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug"],
-  datasets: [
-    {
-      label: "Sales",
-      data: [10, 19, 20, 45, 42, 160, 111, 180],
-      borderColor: "#8fc2f7",
-      backgroundColor: "lightblue",
-    },
-  ],
-};
-const lineOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-};
+// const lineData = {
+//   labels: ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug"],
+//   datasets: [
+//     {
+//       label: "Sales",
+//       data: [10, 19, 20, 45, 42, 160, 111, 180],
+//       borderColor: "#8fc2f7",
+//       backgroundColor: "lightblue",
+//     },
+//   ],
+// };
+// const lineOptions = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       display: false,
+//     },
+//   },
+// };
 
-const DriverScoreCard = () => {
-  return (
-    <div className='mr-4 border shadow-lg w-[430px] h-[435px] border-[#FFFFFFE8] bg-[#ECF0F9] px-2 rounded-2xl'>
-      <div className='px-3 text-2xl py-2 font-semibold border-[#FFFFFF99] border-b '>
-        Driving Score & Risk Analysis
-      </div>
-      <div className='relative'>
-        <div className='absolute top-8 opacity-80 left-23 bg-zinc-200 w-25 h-25 rounded-full flex flex-col justify-center items-center'>
-          <p>Risk</p><p> Distribution</p></div>
-        <div className=' flex justify-center w-full h-[160px] '>
-          <Pie options={pieOptions} data={PieData} />
-        </div>
-      </div>
+// const DriverScoreCard = () => {
+//   return (
+//     <div className='mr-4 border shadow-lg w-[430px] h-[435px] border-[#FFFFFFE8] bg-[#ECF0F9] px-2 rounded-2xl'>
+//       <div className='px-3 text-2xl py-2 font-semibold border-[#FFFFFF99] border-b '>
+//         Driving Score & Risk Analysis
+//       </div>
+//       <div className='relative'>
+//         <div className='absolute top-8 opacity-80 left-23 bg-zinc-200 w-25 h-25 rounded-full flex flex-col justify-center items-center'>
+//           <p>Risk</p><p> Distribution</p></div>
+//         <div className=' flex justify-center w-full h-[160px] '>
+//           <Pie options={pieOptions} data={PieData} />
+//         </div>
+//       </div>
 
-      <div className='px-3 text-2xl py-2 font-semibold border-[#FFFFFF99] border-b '>
-        Average Driving last 8 months
-      </div>
-      <div className='flex justify-center w-full h-[160px] '>
-        <Line options={lineOptions} data={lineData} />
-      </div>
-    </div >
-  );
-}
+//       <div className='px-3 text-2xl py-2 font-semibold border-[#FFFFFF99] border-b '>
+//         Average Driving last 8 months
+//       </div>
+//       <div className='flex justify-center w-full h-[160px] '>
+//         <Line options={lineOptions} data={lineData} />
+//       </div>
+//     </div >
+//   );
+// }
 
-export default DriverScoreCard
+// export default DriverScoreCard
